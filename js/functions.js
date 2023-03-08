2b2aeec735f9360eb20659ed9359c1f29fb70f0a
Дмитрий Пальченков <whorich@bk.ru>
2023-03-08 22:01:35 +0300

    делаем код читабельным

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,30 @@
-const isMaxLenght = (string, maxLength) => string.lenght <= maxLength;
+const isStringMaxLenght = (string, maxLength) => string.lenght <= maxLength;
 
-isMaxLenght('проверяемая строка', 20);
+isStringMaxLenght ('проверяемая строка', 20);
 
-const isPalindrome = (string) => {
-  string = string
+const isPalindrome = (text) => {
+  const rawStr = text
     .replaceAll(' ', '')
     .toLowerCase();
 
-  return string === string.split(' ').reverse().join('');
+  return rawStr === text.split(' ').reverse().join('');
 };
-isPalindrome('Клоп Лёва вёл полк');
 
+isPalindrome('Клоп Лёва вёл полк');
 
 // Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого
 // положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN
-const returnNumber = (string) => {
-  if (typeof string === 'number') {
-    string = String(string);
+const returnNumber = (text) => {
+  const pureStr = text;
+  if (pureStr === 'number') {
+    text = pureStr;
   }
+
   let result = '';
-  for (let i = 0; i < string.length; i++) {
-    if (!Number.isNaN(parseInt(string.at(i), 10))) {
-      result += string.at(i);
+
+  for (let i = 0; i < text.length; i++) {
+    if (!Number.isNaN(parseInt(text.at(i), 10))) {
+      result += text.at(i);
     }
   }
   return parseInt(result, 10);
@@ -29,12 +32,12 @@ const returnNumber = (string) => {
 
 returnNumber('2023 год');
 
-const myPadStart = (string, minLength, pad) => {
-  const actualPad = minLength - string.length;
+const myPadStart = (text, minLength, pad) => {
+  const actualPad = minLength - text.length;
   if (actualPad <= 0) {
-    return string;
+    return text;
   }
-  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
+  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + text;
 };
 
 myPadStart ('1', 2, '0');
